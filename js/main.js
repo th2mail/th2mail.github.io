a88a868972530b4e692a858540d59e64e12d2b56
th2mail <th2mail80@gmail.com>
2021-11-05 22:13:21 +0900

    .

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ $("#dashboard-3").load("/view/Dashboard/Dashboard-3.html");
 $("#dashboard-4").load("/view/Dashboard/Dashboard-4.html");
 $("#dashboard-5").load("/view/Dashboard/Dashboard-5.html");
 $("#dashboard-6").load("/view/Dashboard/dashboard-6.html");
-$("#dashboard-7").load("/view/Dashboard/dashboard-7.html");
+$("#dashboard-7").load("/view/Dashboard/dashboard-7.html");     
 
 // 센서 데이터 현황
 $("#data-status").load("/view/DataPreparation/DataStatus.html");
