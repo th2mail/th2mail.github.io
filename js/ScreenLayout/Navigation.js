64bf69fb95d4f14941cd8471c5922a4909fe7568
th2mail <th2mail80@gmail.com>
2021-11-11 00:52:04 +0900

    Navigation: Tooltip, Item

--- js/ScreenLayout/Navigation.js
+++ js/ScreenLayout/Navigation.js
@@ -14,7 +14,9 @@ const menus = document.querySelectorAll('.menu');
 const close_item = document.querySelector('.rightbar .close');
 const txt_list = document.querySelectorAll('.sidebar ul li a');
 
-// 메뉴를 클릭했을 때 실행되는 이벤트 함수
+/**
+ * 메뉴를 클릭했을 때 실행되는 이벤트 함수
+ */ 
 nav_list.addEventListener('click', (event) => {
 
   // 네비게이션을 연다(Open).
@@ -48,9 +50,35 @@ nav_list.addEventListener('click', (event) => {
 
   // 타이틀을 선택한 메뉴 이름으로 바꾼다.
   document.querySelector('.sidebar .rightbar input.title').value = target.dataset.title;
+
+  // // $('.sidebar .rightbar .item').css({ "display": "none" });
+  // const subItems = document.querySelectorAll('.sidebar .rightbar .item');
+  // subItems.forEach((subItem) => {
+  //   // subItem.style.visibility = "hidden";
+  //   subItem.style.display = "none";
+  // });
+
+  // let subItem;
+  // if (target.dataset.title == `데이터 탐험`) {
+  //   subItem = document.querySelector('.sidebar .rightbar .item.three');
+  // } else if (target.dataset.title == `시계열 데이터 분해`) {
+  //   subItem = document.querySelector('.sidebar .rightbar .item.seven');
+  // } else if (target.dataset.title == `시계열 정상성 검사 및 변환`) {
+  //   subItem = document.querySelector('.sidebar .rightbar .item.eight');
+  // } else if (target.dataset.title == `단변수 예측`) {
+  //   subItem = document.querySelector('.sidebar .rightbar .item.nine');
+  // } else if (target.dataset.title == `다변수 예측`) {
+  //   subItem = document.querySelector('.sidebar .rightbar .item.ten');
+  // } else if (target.dataset.title == `시계열 예측 분석 (차트)`) {
+  //   subItem = document.querySelector('.sidebar .rightbar .item.twelve');    
+  // }
+  // // subItem.style.visibility = "visible";
+  // subItem.style.display = "block";
 });
 
-// 네비게이션의 닫기(<) 버튼을 클릭했을 때 실행되는 이벤트 함수
+/**
+ * 네비게이션의 닫기(<) 버튼을 클릭했을 때 실행되는 이벤트 함수
+ */ 
 close_item.addEventListener('click', (event) => {
   
   // 네비게이션을 닫는다(Close).
