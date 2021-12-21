12a6f0dfeb11cd8bc76b23a8f571c15b96e4a17e
th2mail <th2mail80@gmail.com>
2021-12-22 00:13:35 +0900

    Introduce: CSS 디자인

--- src/main/webapp/assert/js/Dashboard/Introduce.js
+++ src/main/webapp/assert/js/Dashboard/Introduce.js
@@ -7,48 +7,26 @@
  * ---------  -----------------------------------------------------------------
  * 
  */
- $(document).ready(function () {
-
-    $('#introduce .square:nth-child(1)').hover(function () {
-      $('#introduce .card:nth-child(1)').css({
-        'background': 'red'
-      });
-    }, function () {
-        $('#introduce .card:nth-child(1)').css({
-            'background': 'var(--color-whilte)'
-        });
-    });
-
-    $('#introduce .square:nth-child(2)').hover(function () {
-        $('#introduce .card:nth-child(2)').css({
-          'background': 'orange'
-        });
-      }, function () {
-          $('#introduce .card:nth-child(2)').css({
-              'background': 'var(--color-whilte)'
-          });
-      });
-
-      $('#introduce .square:nth-child(3)').hover(function () {
-        $('#introduce .card:nth-child(3)').css({
-          'background': 'purple'
-        });
-      }, function () {
-          $('#introduce .card:nth-child(3)').css({
-              'background': 'var(--color-whilte)'
-          });
-      });
-
-      $('#introduce .square:nth-child(4)').hover(function () {
-        $('#introduce .card:nth-child(4)').css({
-          'background': 'palegreen'
-        });
-      }, function () {
-          $('#introduce .card:nth-child(4)').css({
-              'background': 'var(--color-whilte)'
-          });
-      });
+$(document).ready(function () {
+   
+  /**
+   * 가운데 동그라미 "Alian"에 마우스 오버 했을 때의 처리 이벤트
+   */
+  $('#introduce .square:nth-child(1)').hover(function () { $('#introduce .card:nth-child(1)').css({ 'background': 'rgba(169, 169, 169, 0.123)' }); },
+                                             function () { $('#introduce .card:nth-child(1)').css({ 'background': 'var(--color-white)' });
+  });
+  $('#introduce .square:nth-child(2)').hover(function () { $('#introduce .card:nth-child(2)').css({ 'background': 'rgba(169, 169, 169, 0.123)' }); },
+                                             function () { $('#introduce .card:nth-child(2)').css({ 'background': 'var(--color-white)' });
+  });
+  $('#introduce .square:nth-child(3)').hover(function () { $('#introduce .card:nth-child(3)').css({ 'background': 'rgba(169, 169, 169, 0.123)' }); },
+                                             function () { $('#introduce .card:nth-child(3)').css({ 'background': 'var(--color-white)' });
+  });
+  $('#introduce .square:nth-child(4)').hover(function () { $('#introduce .card:nth-child(4)').css({ 'background': 'rgba(169, 169, 169, 0.123)' }); },
+                                             function () { $('#introduce .card:nth-child(4)').css({ 'background': 'var(--color-white)' });
+    });  
   
+  /**
+   * 
+   */
   
-  
-});
+});//END - document.ready
